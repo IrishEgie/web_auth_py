e822b031758d06e6e002e737e8ae79c5870f519e
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-16 21:36:44 +0800

    update: initialized starting code for web auth project, added registering func

---
+++ main.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# CREATE DATABASE
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# CREATE TABLE IN DB
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Hash the password
+        hashed_password = generate_password_hash(password)
+
+        # Create a new user instance
+        new_user = User(name=name, email=email, password=hashed_password)
+
+        # Add the user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registration successful! You can now log in.", "success")
+        return redirect(url_for('login'))  # Redirect to login page after registration
+
+    return render_template("register.html")
+
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+def secrets():
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/download')
+def download():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
